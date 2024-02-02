96400ece5172e51146a8f2c7ba429179771debd3
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2024-02-02 19:08:33 +0530

    first commit

---
+++ app/src/main/java/com/example/geminichatapp/ChatUiEvent.kt
@@ -0,0 +1,13 @@
+package com.example.geminichatapp
+
+import android.graphics.Bitmap
+
+
+sealed class ChatUiEvent {
+    data class UpdatePrompt(val newPrompt: String) : ChatUiEvent()
+
+    data class SendPrompt(
+        val prompt: String,
+        val bitmap: Bitmap?
+    ) : ChatUiEvent()
+}
---
+++ app/src/main/java/com/example/geminichatapp/Data/ChatData.kt
@@ -0,0 +1,76 @@
+package com.example.geminichatapp.Data
+
+import android.graphics.Bitmap
+import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.content
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+object ChatData {
+
+    val api_key = "AIzaSyC6z1WehyvFIMdnAap3E2iH4MJl5eC0wjs"
+
+
+
+
+
+    suspend fun getResponse(prompt: String): Chat
+    {  // creating mdl for sending prompt and reciving rpns
+        val generativeModel = GenerativeModel(
+            modelName = "gemini-pro", apiKey = api_key
+        )
+
+        try {
+            //genrating and storing response
+            val response = withContext(Dispatchers.IO) {
+                generativeModel.generateContent(prompt)
+            }
+
+            return Chat(
+                prompt = response.text ?: "error",
+                bitmap = null,
+                isFromUser = false
+            )
+
+        } catch (e: Exception) {
+            return Chat(
+                prompt = e.message ?: "error",
+                bitmap = null,
+                isFromUser = false
+            )
+        }
+
+    }
+
+
+
+    suspend fun getResponseWithImage(prompt: String, bitmap: Bitmap): Chat {
+        val generativeModel = GenerativeModel(
+            modelName = "gemini-pro-vision", apiKey = api_key
+        )
+
+        try {
+
+            val inputContent = content {     // combined input prompt
+                image(bitmap)
+                text(prompt)
+            }
+
+            val response = withContext(Dispatchers.IO) {
+                generativeModel.generateContent(inputContent)
+            }
+
+            return Chat(
+                prompt = response.text ?: "error",
+                bitmap = null,
+                isFromUser = false
+            )
+
+        } catch (e: Exception) {
+            return Chat(
+                prompt = e.message ?: "error",
+                bitmap = null,
+                isFromUser = false
+            )
+        }
+}}
